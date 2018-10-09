2893b1558c83873f55f900c15c246de35f82c1b4
openwolf <200550821@qq.com>
2018-10-09 23:40:17 +0800

    java

--- java/hello/src/SimpleGame/SimpleDotCom.java
+++ java/hello/src/SimpleGame/SimpleDotCom.java
@@ -1,14 +1,30 @@
 package SimpleGame;
 
 public class SimpleDotCom {
+	//	存 dotcom 的坐标
 	int [] locationCells;
+	//	记录击中数
 	int numOfHits;
-	String checkYourself(String guess) {
-		
-		return "ok";
+	
+	public String checkYourself(String stringGuess) {
+		//	把字符串转换成int
+		int guess = Integer.parseInt(stringGuess);
+		//	创建出保存返回结果的变量。以 miss 作为默认值
+		String result = "miss";
+		for(int cell : locationCells) {
+			if(guess == cell) {
+				result ="hit";
+				numOfHits++;
+				break;
+			}
+		}
+		if(numOfHits == locationCells.length) {
+			result = "kill";
+		}
+		return result;
 	}
 	
 	void setLocationCells(int[] loc) {
-		
+		locationCells = loc;
 	}
 }
--- java/hello/src/SimpleGame/SimpleDotComGame.java
+++ java/hello/src/SimpleGame/SimpleDotComGame.java
@@ -1,6 +1,6 @@
 package SimpleGame;
 
-public class SimpleGame {
+public class SimpleDotComGame {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
