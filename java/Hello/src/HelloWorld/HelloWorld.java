5ae3c104637202f452e139f3f452bd142ed49c6f
openwolf <200550821@qq.com>
2018-10-22 23:53:51 +0800

    java

--- java/Hello/src/HelloWorld/HelloWorld.java
+++ java/Hello/src/HelloWorld/HelloWorld.java
@@ -4,8 +4,8 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		//	System.out.println("hello world");
-		int a = 99;
+		System.out.println("hello world");
+		byte a = 99;
 		while(a>=0){
 			System.out.println(a);
 			a -= 1;
