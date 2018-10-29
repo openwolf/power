4f2d8940b1da24f9977fa7fe25d1103ca6d923a1
majunjie <majunjie@ycxiaozhan.com>
2018-10-29 13:25:59 +0800

    java

---
+++ java/Hello/src/HelloWorld/UseCar.java
@@ -0,0 +1,19 @@
+package HelloWorld;
+
+public class UseCar {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Car carPassedBy;
+		carPassedBy = new Car();
+		carPassedBy.speed = 60;
+		carPassedBy.color = "白色";
+		carPassedBy.name = "马自达";
+		carPassedBy.direction = "南京方向";
+		System.out.println(carPassedBy.speed);
+		System.out.println(carPassedBy.color);
+		System.out.println(carPassedBy.name);
+		System.out.println(carPassedBy.direction);
+	}
+
+}
