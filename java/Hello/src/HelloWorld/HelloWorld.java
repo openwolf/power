650d88c45818d5ae2b426f7be8d39cd059bf73c0
majunjie <majunjie@ycxiaozhan.com>
2018-11-11 18:32:08 +0800

    java

--- java/Hello/src/HelloWorld/HelloWorld.java
+++ java/Hello/src/HelloWorld/HelloWorld.java
@@ -17,11 +17,31 @@ public class HelloWorld {
 //		int b = a + c;
 //		int c =1;
 //		System.out.println(b);
-		int[] a = {7,8,9};
-		int[] b = a.clone();
-		b[1] = 2;
-		System.out.println("a"+a[1]);
-		System.out.println("b.length:"+b.length);
+//		int[] a = {7,8,9};
+//		int[] b = a.clone();
+//		b[1] = 2;
+//		System.out.println("a"+a[1]);
+//		System.out.println("b.length:"+b.length);
+//		Car a = new Car();
+//		System.out.println(a==null);
+//		String str_0 = "abc";
+//		String str_1 = "abc";
+//		String str_2 = str_0;
+//		System.out.println( "str_0 == str_1 :" + (str_0 == str_1)  );
+//		System.out.println( "str_0 == str_2 :" + (str_0 == str_2)  );
+//		
+//		Car a = new Car();
+//		Car b = new Car();
+//		Car c = a;
+//		System.out.println( "a == b:" + (a==b)  );
+//		System.out.println( "a == c:" + (a==c)  );
+//		
+//		int [] arr_0 = {1,2};
+//		int [] arr_1 = {1,2};
+//		int [] arr_2 = arr_0;
+//		System.out.println("arr_0 == arr_1:" + (arr_0==arr_1));
+//		System.out.println("arr_0 == arr_2:" + (arr_0==arr_2));
+		
 	}
 
 }
