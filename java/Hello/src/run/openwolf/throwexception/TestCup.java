99f5966b3c6c4e1874a35300e4c6468dae350ebc
majunjie <majunjie@ycxiaozhan.com>
2018-12-21 18:50:52 +0800

    同步

--- java/Hello/src/run/openwolf/throwexception/TestCup.java
+++ java/Hello/src/run/openwolf/throwexception/TestCup.java
@@ -5,7 +5,12 @@ public class TestCup {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Cup aCup = new Cup();
-		aCup.setCapacity(0);
+		try {
+			aCup.setCapacity(10001);
+		} catch (Exception e) {
+			System.out.println("捕捉到了exception类型的异常。异常信息如下");
+			System.out.println(e.getMessage());
+			System.out.println("异常处理结束");
+		}
 	}
-
 }
