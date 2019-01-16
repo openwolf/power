e5c0ffe004c7f70ce165c873a472a2cfbe2143be
openwolf <200550821@qq.com>
2019-01-16 21:43:12 +0800

    java

---
+++ java/Hello/src/run/openwolf/string/String_01.java
@@ -0,0 +1,9 @@
+package run.openwolf.string;
+
+public class String_01 {
+	public static void main(String[] args) {
+		String s0 = new String("hello");
+		String s1 = "hello";
+		System.out.println(s0.equals(s1));
+	}
+}
