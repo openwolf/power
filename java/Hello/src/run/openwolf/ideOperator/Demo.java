8c860821c2eff55be53e753b5552159706ff0403
majunjie <majunjie@ycxiaozhan.com>
2019-01-17 13:57:31 +0800

    同步

---
+++ java/Hello/src/run/openwolf/ideOperator/Demo.java
@@ -0,0 +1,28 @@
+package run.openwolf.ideOperator;
+
+public class Demo {
+	private int num;
+	private String name;
+	public Demo() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public Demo(int num, String name) {
+		super();
+		this.num = num;
+		this.name = name;
+	}
+	public int getNum() {
+		return num;
+	}
+	public void setNum(int num) {
+		this.num = num;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
