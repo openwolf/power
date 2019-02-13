e7ceca2b86acb78dbad01ae64cd718ab767ffee1
majunjie <majunjie@ycxiaozhan.com>
2019-02-13 16:38:47 +0800

    java

--- java/Hello/src/run/openwolf/exceptionDemo/Demo1.java
+++ java/Hello/src/run/openwolf/exceptionDemo/Demo1.java
@@ -5,13 +5,14 @@ public class Demo1 {
 		try {
 			System.out.println(2/0);
 		} catch (Exception e) {
-			System.out.println(e.getMessage());
-			System.out.println(e.toString());
-			e.printStackTrace();
+//			System.out.println(e.getMessage());
+//			System.out.println(e.toString());
+			e.printStackTrace(System.out);
 			System.out.println(1);
-		} finally {
 			System.out.println(2);
+		} finally {
+			System.out.println(3);
 		}
-		System.out.println(3);
+		System.out.println(4);
 	}
 }
