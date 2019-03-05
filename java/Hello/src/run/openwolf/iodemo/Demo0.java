73ab8816fd4b986896937cd286877b85155904cc
openwolf <200550821@qq.com>
2019-03-05 23:54:45 +0800

    java

---
+++ java/Hello/src/run/openwolf/iodemo/Demo0.java
@@ -0,0 +1,14 @@
+package run.openwolf.iodemo;
+
+import java.io.File;
+import java.util.Arrays;
+
+public class Demo0 {
+	public static void main(String[] args) {
+		File path = new File(".");
+		String[] list;
+		list = path.list();
+		System.out.println(list);
+		System.out.println(Arrays.toString(list));
+	}
+}
