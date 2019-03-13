5265603ada40d1b085e7dd53c98839daa0e3bedc
openwolf <200550821@qq.com>
2019-03-13 23:51:18 +0800

    perl

--- perl/test/subroutine.pl
+++ perl/test/subroutine.pl
@@ -1,9 +1,19 @@
 use strict;
 use warnings;
+
 sub sub0{
 	print("hello world!");
-	$var0 = 11;
+	my $var0 = 11;
 }
-sub0();
+# &sub0();
 
-print("$var0\n");
+sub sum{
+	print("@_\n");
+	my $sum = 0;
+	foreach (@_) {
+		$sum += $_;
+	}
+	print("$sum\n");
+}
+my @arr = (1..100);
+&sum(@arr,1);
