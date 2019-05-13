8576922ac4eea0fc847597667b76a76fb404051b
openwolf <200550821@qq.com>
2019-05-13 22:32:00 +0800

    删除近期不学习的项目

--- python/01.py
+++
@@ -1,5 +0,0 @@
-print('hello world')
-print(type('小明'))
-print(type(True))
-print(type(12))
-print(type(12.1))
--- python/02.py
+++
@@ -1,6 +0,0 @@
-# coding=utf-8
-x = 10
-y = float(input('请输入数字'))
-s = x * y
-print(s, y)
-print('hello\nworld')
