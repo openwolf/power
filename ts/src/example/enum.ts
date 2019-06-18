105cb10c7a8a74df0a105ef6571d84e1235d5727
openwolf <200550821@qq.com>
2019-06-19 00:10:46 +0800

    ts

---
+++ ts/src/example/enum.ts
@@ -0,0 +1,9 @@
+enum Status {
+	Pending,
+	Resolve,
+	Reject,
+};
+
+console.log(Status.Pending);
+console.log(Status.Resolve);
+console.log(Status.Reject);
--- ts/src/index.ts
+++ ts/src/index.ts
@@ -4,5 +4,7 @@
 // import './example/function';
 // import './example/generics';
 // import './example/es6-class-0';
-import './example/class';
+// import './example/class';
+// import './example/enum';
+import './example/inference-compatibility';
 console.log('hello index.ts');
